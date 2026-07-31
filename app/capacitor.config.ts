ebc239df8e298e9c238cbfe0d6baf539fd43f023
mini_shell <cjsan30@gmail.com>
2026-07-31 21:34:04 +0900

    Initialize hybrid app foundation

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.cjsan30.shinhanhae.calculator',
+  appName: '신청해 계산기',
+  webDir: 'dist',
+};
+
+export default config;
