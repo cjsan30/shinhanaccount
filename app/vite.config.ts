9f8335c634ec683836515fb14eaa9d990b887f96
mini_shell <cjsan30@gmail.com>
2026-08-01 17:12:15 +0900

    Expose test tools in internal builds

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_ENABLE_TEST_TOOLS': JSON.stringify(mode === 'test' ? 'true' : ''),
+    'import.meta.env.VITE_ENABLE_TEST_TOOLS': JSON.stringify(mode === 'test' || mode === 'internal' ? 'true' : ''),
   },
   test: {
     environment: 'jsdom',
